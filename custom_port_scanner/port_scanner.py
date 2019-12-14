89ea72d30647cc73f5939b36b7b2680101cd31bb
KW <45307694+Asienwald@users.noreply.github.com>
2019-12-14 12:35:32 +0800

    Add files via upload

--- custom_port_scanner/port_scanner.py
+++ custom_port_scanner/port_scanner.py
@@ -1,48 +1,61 @@
 import socket
 import os
-import threading
+from threading import Thread, Lock
 import re
 import time
-import datetime as dt
 
 # common ports to use
 COMMON_PORTS = (20, 21, 22, 23, 25, 53, 67, 68, 80, 110, 135, 139, 143, 
                 443, 445, 3389)
 
-def scan_port(target_ip, port):
-    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-    socket.setdefaulttimeout(1)
-    conn = s.connect_ex((target_ip, port))
+lock = Lock()
+threads = []
 
-    if conn == 0:
-        print(f"Port {port} is open.", end="\n")
-        time.sleep(1)
-    # else:
-    #     print("nope")
+open_ports = []
+
+
+
+def scan_port(target_ip, port):
+    try:
+        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        socket.setdefaulttimeout(1)
+        conn = s.connect_ex((target_ip, port))
+
+        if conn == 0:
+            lock.acquire()
+            open_ports.append(port)
+            lock.release()
+    except OSError:
+        pass
 
 def scan_ports(target_ip, scan_common: bool, scan_start, scan_end):
-    # start_time = time.process_time()
-    start_time = dt.datetime.now()
+    start_time = time.time()
 
     print(f"Scanning host at {target_ip}...", end="\n\n")
-    
     
     if scan_common:
         print("Scanning for common ports...")
         for port in COMMON_PORTS:
-            t = threading.Thread(target=scan_port, args=(target_ip, port))
-            t.start()
-            time.sleep(.1)
+            t = Thread(target=scan_port, args=(target_ip, port))
+            threads.append(t)
+            threads[-1].start()
+            
         print("\n")
+    else:
+        print(f"Scanning ports {scan_start} to {scan_end}...")
+        for port in range(scan_start, scan_end):
+            t = Thread(target=scan_port, args=(target_ip, port))
+            threads.append(t)
+            threads[-1].start()
+            
 
-    print(f"Scanning ports {scan_start} to {scan_end}...")
-    for port in range(scan_start, scan_end):
-        t = threading.Thread(target=scan_port, args=(target_ip, port))
-        t.start()
-        time.sleep(.01)  
+    for thread in threads:
+        thread.join()
 
-    # elapsed time calculation doesn't work when using multithreading
-    elapsed_time = (dt.datetime.now() - start_time).total_seconds()
+    for port in open_ports:
+        print(f"Port {port} is open.")
+
+    elapsed_time = time.time() - start_time
     print(f"\nPort scan finished.\nTime elapsed: {elapsed_time:.3f} seconds.")
     
     os._exit(1)
@@ -65,27 +78,29 @@ def main():
         else:
             print("Please enter valid IP.", end="\n\n")
     while True:
-        scan_common = input("Do you want to scan for common ports? (y/n): ")
+        scan_common = input("Do you want to scan for common ports only? (y/n): ")
         if re.match('[yY]', scan_common):
             scan_common = True
+            scan_start, scan_end = 0, 0
             break
         elif re.match('[nN]', scan_common):
             scan_common= False
+            while True:
+                scan_range = input("Input port range to scan (1-65535): ")
+                if re.match('\d+-\d+', scan_range):
+                    scan_range = scan_range.split("-")
+                    scan_start, scan_end = int(scan_range[0]), int(scan_range[1])
+                    if scan_start > scan_end:
+                        print("Input valid range.", end="\n\n")
+                        continue
+                    else:
+                        break
+                else:
+                    print("Please enter valid range of x-y.", end="\n\n")
             break
         else:
             print("Please input a valid response.", end="\n\n")
-    while True:
-        scan_range = input("Input port range to scan (1-65535): ")
-        if re.match('\d+-\d+', scan_range):
-            scan_range = scan_range.split("-")
-            scan_start, scan_end = int(scan_range[0]), int(scan_range[1])
-            if scan_start > scan_end:
-                print("Input valid range.", end="\n\n")
-                continue
-            else:
-                break
-        else:
-            print("Please enter valid range of x-y.", end="\n\n")
+    
     # begin port scans
     scan_ports(target_ip, scan_common, scan_start, scan_end)
 
